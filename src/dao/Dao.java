5a6de4bc59640cc2e0966afbe2ecddc513b498b6
Felipe Umpierre <felipeumpierre@hotmail.com>
2015-07-02 18:23:43 -0300

    Vies

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -5,54 +5,40 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public abstract class Dao
-{
-	private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	private static final String DB_URL = "jdbc:mysql://localhost:3306/restaurante";
-	private static final String ID = "root";
-	private static final String PASS = "";
+public abstract class Dao {
 
-	public Connection getConnection()
-	{
-		try 
-		{
-			Class.forName( DRIVER_NAME );
-			
-			return DriverManager.getConnection( DB_URL, ID, PASS );
-		}
-		catch( Exception e ) 
-		{
-			throw new RuntimeException( e );
-		}
-	}
+   private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+   private static final String DB_URL = "jdbc:mysql://localhost:3306/restaurante";
+   private static final String ID = "root";
+   private static final String PASS = "";
 
-	public static void close( Connection con )
-	{
-		if( con != null ) 
-		{
-			try
-			{
-				con.close();
-			}
-			catch( SQLException e )
-			{
-				throw new RuntimeException(e);
-			}
-		}
-	}
+   public Connection getConnection() {
+      try {
+         Class.forName(DRIVER_NAME);
 
-	public static void close( Statement stmt )
-	{
-		if( stmt != null )
-		{
-			try 
-			{
-				stmt.close();
-			}
-			catch( SQLException e )
-			{
-				throw new RuntimeException(e);
-			}
-		}
-	}
+         return DriverManager.getConnection(DB_URL, ID, PASS);
+      } catch (Exception e) {
+         throw new RuntimeException(e);
+      }
+   }
+
+   public static void close(Connection con) {
+      if (con != null) {
+         try {
+            con.close();
+         } catch (SQLException e) {
+            throw new RuntimeException(e);
+         }
+      }
+   }
+
+   public static void close(Statement stmt) {
+      if (stmt != null) {
+         try {
+            stmt.close();
+         } catch (SQLException e) {
+            throw new RuntimeException(e);
+         }
+      }
+   }
 }
--- src/facade/FacadeTable.java
+++ src/facade/FacadeTable.java
@@ -6,38 +6,39 @@ import dao.TableDaoImpl;
 import entity.Table;
 
 public class FacadeTable {
-	protected TableDaoImpl table;
-	
-	public FacadeTable() {
-		table = new TableDaoImpl();
-	}
-	
-	public Table insert(int number, int capacity) {
-		Table t = new Table(number, capacity);
-		table.insert(t);
-		
-		return t;
-	}
 
-	public Table update(int number, int capacity) {
-		Table t = table.findByNumber(number);
-		t.setCapacity(capacity);
-		table.update(t);
-		
-		return t;
-	}
-	
-	public void delete(int number) {
-		Table t = table.findByNumber(number);
-		
-		table.delete(t);
-	}
-	
-	public List<Table> listAll() {
-		return table.findAll();
-	}
-	
-	public Table listByNumber(int number) {
-		return table.findByNumber(number);
-	}
+   protected TableDaoImpl table;
+
+   public FacadeTable() {
+      table = new TableDaoImpl();
+   }
+
+   public Table insert(int number, int capacity) {
+      Table t = new Table(number, capacity);
+      table.insert(t);
+
+      return t;
+   }
+
+   public Table update(int number, int capacity) {
+      Table t = table.findByNumber(number);
+      t.setCapacity(capacity);
+      table.update(t);
+
+      return t;
+   }
+
+   public void delete(int number) {
+      Table t = table.findByNumber(number);
+
+      table.delete(t);
+   }
+
+   public List<Table> listAll() {
+      return table.findAll();
+   }
+
+   public Table listByNumber(int number) {
+      return table.findByNumber(number);
+   }
 }
